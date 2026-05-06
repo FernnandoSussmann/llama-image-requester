866cc3621af125d5c5151557f2f8ee369bc15595
Fernnando Sussmann <fernnandosussmann@gmail.com>
2026-05-06 15:56:40 -0300

    Fixing variable reference

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 IMAGE_NAME=llama-image-requester
 PROJECT_IMAGE=$(docker image ls --format "{{.Repository}}" | grep $IMAGE_NAME)
 
-if [[ -z "$model" ]]; then
+if [[ -z "$PROJECT_IMAGE" ]]; then
     docker build -t $IMAGE_NAME .
 fi
 
